f0aa23a6c2e330d1ee39605f0da8e4b4427eb294
Bikash <bikash11roy@gmail.com>
2017-02-20 00:25:38 +0600

    Getting the list of Drives

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import string
+from ctypes import windll
+import time
+import os
+
+
+def get_drives():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+    return drives
+
+print(get_drives())
