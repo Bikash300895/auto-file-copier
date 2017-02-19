ce8ff519c04f03f5e4b8ad2ae364a8ed63f000b4
Bikash <bikash11roy@gmail.com>
2017-02-20 05:01:28 +0600

    Running functionality added

--- src/main.py
+++ src/main.py
@@ -1,12 +1,44 @@
 import string
 from ctypes import windll
 import time
+import shutil
 import os
-from src.copy_to import CopyTo
+# from src.copy_to import CopyTo
 
 directory = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
 prev_files = []
 toCopy = []
+
+
+def copy_file(files, drive):
+    for file in files:
+        if os.path.isdir(file):
+            try:
+                copytree(file, drive + ":\\")
+            except:
+                print("copying directory error")
+        else:
+            try:
+                copyfile(file, drive + ":\\")
+            except:
+                print("Copying file error")
+
+
+def copyfile(file, drive):
+    shutil.copy(file, drive)
+    print("Copying")
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+        print("Copying")
+
 
 
 def get_drives():
@@ -67,7 +99,7 @@ while(True):
 
     for drive in new_drive:
         print("New drive detected: "+ drive)
-        CopyTo.copy_file(toCopy, drive)
+        copy_file(toCopy, drive)
 
     prev_dirve = current_drive
 
